fc8681f23fc1cbae46f6947396fb626e4148c521
Mikael Lund <Mlund@mac.com>
2025-01-17 08:57:45 +0000

    Clean up virial

--- src/report.rs
+++ src/report.rs
@@ -1,16 +1,101 @@
 use crate::Sample;
 use coulomb::Vector3;
+use itertools::Itertools;
 use nu_ansi_term::Color::{Red, Yellow};
 use num_traits::Inv;
 use physical_constants::AVOGADRO_CONSTANT;
 use rgb::RGB8;
 use std::{
+    f64::consts::PI,
     fs::File,
     io::Write,
     ops::{Add, Mul, Neg},
     path::PathBuf,
 };
 use textplots::{Chart, ColorPlot, Shape};
+
+/// Helper struct for handling osmotic second virial coefficients, B2
+#[derive(Debug, Clone)]
+pub struct VirialCoeff {
+    /// Osmotic second virial coefficient, B2 (Å³)
+    b2: f64,
+    /// Distance of closest approach, σ (Å)
+    sigma: f64,
+}
+
+impl From<VirialCoeff> for f64 {
+    fn from(v: VirialCoeff) -> f64 {
+        v.b2
+    }
+}
+
+impl VirialCoeff {
+    // pub fn from_pmf_iterator(pomf: impl Iterator<Item = (f32, f32)>) -> Self {
+    //     Self::from_pmf(&pomf.collect::<Vec<_>>())
+    // }
+    /// Calculate B2 from PMF data, pairs of (r, w(r)).
+    /// w(r) should be in units of kT; r should be equidistant and in Å.
+    /// Now calculate the osmotic second virial coefficient by integration:
+    /// 𝐵₂ = -½ ∫ [ exp(-𝛽𝑤(𝑟) ) - 1 ] 4π𝑟² d𝑟
+    pub fn from_pmf(pomf: &[(f32, f32)]) -> anyhow::Result<Self> {
+        // use first two distances to calculate dr
+        let mut iter = pomf.iter();
+        let (r0, r1) = iter
+            .by_ref()
+            .map(|pair| pair.0)
+            .take(2)
+            .collect_tuple()
+            .ok_or_else(|| anyhow::anyhow!("Error calculating PMF dr"))?;
+        let dr = (r1 - r0) as f64;
+        if dr <= 0.0 {
+            anyhow::bail!("Negative dr in PMF");
+        }
+        let sigma = r0 as f64; // closest distance, "σ"
+        let b2_hardsphere = 2.0 * PI / 3.0 * sigma.powi(3);
+        // integrate
+        let b2 = iter
+            .map(|(r, w)| (*r as f64, *w as f64))
+            .map(|(r, w)| w.neg().exp_m1() * r * r)
+            .sum::<f64>()
+            .mul(-2.0 * PI * dr)
+            .add(b2_hardsphere);
+        Ok(Self { b2, sigma })
+    }
+    /// Distance of closest approach, σ (Å)
+    pub fn sigma(&self) -> f64 {
+        self.sigma
+    }
+    /// Hard sphere contribution to second virial coefficient, B2hs (Å³)
+    pub fn hardsphere(&self) -> f64 {
+        2.0 * PI / 3.0 * self.sigma.powi(3)
+    }
+    /// Reduced second virial coefficient, B2 / B2hs
+    pub fn reduced(&self) -> f64 {
+        self.b2 / self.hardsphere()
+    }
+    /// Association constant, 𝐾𝑑⁻¹ = -2(𝐵₂ - 𝐵₂hs)
+    /// See "Colloidal Domain" by Evans and Wennerström, 2nd Ed, p. 408
+    pub fn association_const(&self) -> Option<f64> {
+        const LITER_PER_CUBIC_ANGSTROM: f64 = 1e-27;
+        let association_const =
+            -2.0 * (self.b2 - self.hardsphere()) * LITER_PER_CUBIC_ANGSTROM * AVOGADRO_CONSTANT;
+        if association_const.is_sign_positive() {
+            Some(association_const)
+        } else {
+            None
+        }
+    }
+    /// Dissociation constant, 𝐾𝑑
+    /// See "Colloidal Domain" by Evans and Wennerström, 2nd Ed, p. 408
+    pub fn dissociation_const(&self) -> Option<f64> {
+        self.association_const().map(|k| k.inv())
+    }
+    /// Virial coefficient, B2 in mol⋅ml/g². Molar weights in g/mol.
+    pub fn mol_ml_per_gram2(&self, mw1: f64, mw2: f64) -> f64 {
+        const ML_PER_ANGSTROM3: f64 = 1e-24;
+        self.b2 * ML_PER_ANGSTROM3 / (mw1 * mw2) * AVOGADRO_CONSTANT
+    }
+}
 
 /// Write PMF and mean energy as a function of mass center separation to file
 pub fn report_pmf(
@@ -41,46 +126,30 @@ pub fn report_pmf(
         }
     });
 
-    // Now calculate the osmotic second virial coefficient by integrating `pmf_data`, w(r)
-    // 𝐵₂ = -½ ∫ [ exp(-𝛽𝑤(𝑟) ) - 1 ] 4π𝑟² d𝑟
-    let (r0, r1) = (pmf_data[0].0, pmf_data[1].0);
-    let dr = (r1 - r0) as f64;
-    assert!(dr > 0.0);
-    let closest_approach = r0 as f64; // "σ"
-    use std::f64::consts::PI;
-    let b2_hardsphere = 2.0 * PI / 3.0 * closest_approach.powi(3);
-    let b2 = pmf_data
-        .iter()
-        .map(|(r, w)| (*r as f64, *w as f64))
-        .map(|(r, w)| w.neg().exp_m1() * r * r)
-        .sum::<f64>()
-        .mul(-2.0 * PI * dr)
-        .add(b2_hardsphere);
-    info!("Second virial coefficient, 𝐵₂ = {:.2} Å³", b2);
+    let virial = VirialCoeff::from_pmf(&pmf_data)?;
+
+    info!(
+        "Second virial coefficient, 𝐵₂ = {:.2} Å³",
+        f64::from(virial.clone())
+    );
     if let Some((mw1, mw2)) = masses {
-        const ML_PER_ANGSTROM3: f64 = 1e-24;
         info!(
             "                              = {:.2e} mol⋅ml/g²",
-            b2 * ML_PER_ANGSTROM3 / (mw1 * mw2) * AVOGADRO_CONSTANT
+            virial.mol_ml_per_gram2(mw1, mw2)
         );
     }
 
     info!(
         "Reduced second virial coefficient, 𝐵₂ / 𝐵₂hs = {:.2} using σ = {:.2} Å",
-        b2 / b2_hardsphere,
-        closest_approach
+        virial.reduced(),
+        virial.sigma()
     );
 
-    // See "Colloidal Domain" by Evans and Wennerström, 2nd Ed, p. 408
-    // 𝐾𝑑⁻¹ = -2(𝐵₂ - 𝐵₂hs)
-    const LITER_PER_CUBIC_ANGSTROM: f64 = 1e-27;
-    let association_const =
-        -2.0 * (b2 - b2_hardsphere) * LITER_PER_CUBIC_ANGSTROM * AVOGADRO_CONSTANT;
-    if association_const.is_sign_positive() {
+    if let Some(kd) = virial.dissociation_const() {
         info!(
             "Dissociation constant, 𝐾𝑑 = {:.2e} mol/l using σ = {:.2} Å",
-            association_const.inv(),
-            closest_approach
+            kd,
+            virial.sigma()
         );
     }
 
