24ac1fe88e2b308feba6c72744f0d0d5438ea748
mlund <mlund@localhost>
2025-03-19 16:43:55 +0100

    Add area calculation to example

--- examples/lookup.rs
+++ examples/lookup.rs
@@ -1,7 +1,7 @@
 use duello::IcoSphere;
 use hexasphere::AdjacencyBuilder;
 fn main() {
-    let n_divisions = 0;
+    let n_divisions = 1;
     let icosphere = IcoSphere::new(n_divisions, |_| ());
 
     let indices = icosphere.get_all_indices();
@@ -18,6 +18,17 @@ fn main() {
     for (i, triangle) in indices.chunks(3).enumerate() {
         println!("{} [{}, {}, {}]", i, triangle[0], triangle[1], triangle[2],);
     }
+
+    println!("\nFaces areas:");
+    for (n, triangle) in indices.chunks(3).enumerate() {
+        let a = vertices[triangle[0] as usize];
+        let b = vertices[triangle[1] as usize];
+        let c = vertices[triangle[2] as usize];
+        let ab = b - a;
+        let ac = c - a;
+        println!("Area of face {}: {}", n, 0.5 * ab.cross(ac).length());
+    }
+
     let mut ab = AdjacencyBuilder::new(vertices.len());
     ab.add_indices(&indices);
     let adjency = ab.finish();
