122e0b4a8933a7adf7f60ab4f86c583cef468aec
mlund <mlund@localhost>
2025-06-11 16:30:17 +0200

    Add sidechain option to pdb2xyz

--- scripts/pdb2xyz.py
+++ scripts/pdb2xyz.py
@@ -19,17 +19,46 @@ def convert_pdb(pdb_file, output_xyz_file):
             mw = mw + a.element.mass
         cm = cm / mw * 10.0
         residues.append(dict(name=res.name, cm=cm))
+
+        if "sidechains" in sys.argv:
+            side_chain = add_sidechains(traj, res)
+            if side_chain is not None:
+                residues.append(side_chain)
+
     with open(output_xyz_file, "w") as f:
         f.write(f"{len(residues)}\n")
+        f.write(f"Converted with Duello pdb2xyz.py using input file {pdb_file}\n");
         for i in residues:
-            f.write(f"{i['name']} {i['cm'][0]} {i['cm'][1]} {i['cm'][2]}\n")
+            f.write(f"{i['name']} {i['cm'][0]:.3f} {i['cm'][1]:.3f} {i['cm'][2]:.3f}\n")
     print(f"Converted {pdb_file} -> {output_xyz_file} with {len(residues)} residues.")
+
+
+def add_sidechains(traj, res):
+    for atom in res.atoms:
+        if res.name == "ASP" and atom.name == "OD1":
+            return dict(name="COO-", cm=traj.xyz[0][atom.index] * 10)
+        elif res.name == "GLU" and atom.name == "OE1":
+            return dict(name="COO-", cm=traj.xyz[0][atom.index] * 10)
+        elif res.name == "ARG" and atom.name == "CZ":
+            return dict(name="GDN+", cm=traj.xyz[0][atom.index] * 10)
+        elif res.name == "LYS" and atom.name == "NZ":
+            return dict(name="NH3+", cm=traj.xyz[0][atom.index] * 10)
+    return None
 
 
 if __name__ == "__main__":
     import sys
 
     if len(sys.argv) < 3:
-        print("Usage: python pdb2xyz.py input.pdb output.xyz")
+        print("Usage: python pdb2xyz.py input.pdb output.xyz [--sidechains]")
         sys.exit(1)
-    convert_pdb(sys.argv[1], sys.argv[2])
+
+    sidechains = "--sidechains" in sys.argv
+    pdb_file = sys.argv[1]
+    output_xyz_file = sys.argv[2]
+
+    # Pass the sidechains flag via sys.argv for compatibility with existing code
+    if sidechains and "sidechains" not in sys.argv:
+        sys.argv.append("sidechains")
+
+    convert_pdb(pdb_file, output_xyz_file)
