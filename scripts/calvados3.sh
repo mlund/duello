67ffc2662974b7a0eb8018e6600ab5d2b8213db4
mlund <mlund@localhost>
2025-07-01 13:32:41 +0200

    Update calvados run script to lower resolution

--- scripts/calvados3.sh
+++ scripts/calvados3.sh
@@ -3,13 +3,13 @@
 example=$(basename "$0" .sh)
 cd examples/${example}
 
-RUST_LOG="Info" cargo run --release \
+RUST_LOG="Debug" cargo run --release \
     -- scan \
     -1 4lzt.xyz \
     -2 4lzt.xyz \
     --rmin 24 --rmax 80 --dr 0.5 \
     --top topology.yaml \
-    --resolution 0.5 \
+    --resolution 0.7 \
     --cutoff 1000 \
     --molarity 0.05 \
     --temperature 298.15
